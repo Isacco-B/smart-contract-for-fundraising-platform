220f77b44cd9436e4765a4b8ccb6a5bf6f785f39
Isacco-B <git@isaccobertoli.com>
2023-10-28 19:40:21 +0200

    first commit

---
+++ Crowdfunding.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+contract Crowdfunding {
+    address public manager;
+    uint public totalBalance;
+    uint public goal;
+    uint public donators;
+    bool public fundsTransferred = false;
+    enum Statuses {
+        Open,
+        Close
+    }
+    Statuses public currentStatus;
+
+    constructor(uint _goal) {
+        manager = msg.sender;
+        goal = _goal;
+        currentStatus = Statuses.Open;
+    }
+
+    modifier isManager() {
+        require(manager == msg.sender, "Only the manager can call this function");
+        _;
+    }
+
+    modifier isOpen() {
+        require(currentStatus == Statuses.Open, "The CroudFounding campaign is closed");
+        _;
+    }
+
+    function donate() public payable isOpen {
+        require(msg.value > 0, "Donation amount must be greater than 0");
+        totalBalance += msg.value;
+        donators++;
+    }
+
+    function withdrawFound() public isManager {
+        require(currentStatus == Statuses.Close, "The CroudFounding campaign is not closed");
+        require(!fundsTransferred, "Funds already transferred");
+        (bool success, ) = payable(manager).call{value: totalBalance}("");
+        require(success, "Transfer failed");
+
+        fundsTransferred = true;
+    }
+
+    function closeCampain() public isManager isOpen{
+        require(checkGoalReached(), "Goal not reached yet");
+        currentStatus = Statuses.Close;
+    }
+
+    function checkGoalReached() public view returns (bool) {
+        return totalBalance >= goal;
+    }
+
+    function startNewCampain(uint _goal) public isManager {
+        require(currentStatus == Statuses.Close, "The CroudFounding campaign is not closed");
+        require(fundsTransferred, "the funds have not yet been transferred");
+        totalBalance = 0;
+        donators = 0;
+        goal = _goal;
+        currentStatus = Statuses.Open;
+        fundsTransferred = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Start2Impact_Progetto_Intro_Blockchain
